212c64438699f4f15afdb2aaa9bcbea5f50fdc6b
moka <jana12moumita@gmail.com>
2023-04-25 16:48:03 +0530

    Added: draft code for labelme data

---
+++ imgaug2labelme.py
@@ -0,0 +1,115 @@
+# import json
+# from imgaug.augmentables.polys import Polygon, PolygonsOnImage
+
+# # create a PolygonsOnImage object
+# polygon = Polygon([(0,0), (100,0), (100,100), (0,100)])
+# polys = PolygonsOnImage([polygon], shape=(256, 256, 3))
+
+# # convert the PolygonsOnImage object to list of coordinates
+# coords = polys.to_xy_array()
+
+# # create the labelme-compatible dictionary
+# labelme_dict = {
+#     "version": "4.5.7",
+#     "flags": {},
+#     "shapes": [
+#         {
+#             "label": "polygon",
+#             "points": coords.tolist(),
+#             "group_id": None,
+#             "shape_type": "polygon",
+#             "flags": {}
+#         }
+#     ],
+#     "imagePath": "path/to/image/file.jpg",
+#     "imageData": None,
+#     "imageHeight": 256,
+#     "imageWidth": 256
+# }
+
+# # save the labelme-compatible dictionary to a file
+# with open("labelme_data.json", "w") as f:
+#     json.dump(labelme_dict, f)
+
+
+import json
+from imgaug.augmentables.polys import Polygon, PolygonsOnImage
+'''
+# create PolygonsOnImage object with multiple Polygon objects
+polygons = [Polygon([(0,0), (100,0), (100,100), (0,100)]), Polygon([(50,50), (75,50), (75,75), (50,75)])]
+polys = PolygonsOnImage(polygons, shape=(256, 256, 3))
+
+# convert the list of polygons to a list of lists of coordinates
+coords = [[[(x, y) for (x, y) in polygon.exterior]] for polygon in polygons]
+
+# create the labelme-compatible dictionary
+labelme_dict = {
+    "version": "4.5.7",
+    "flags": {},
+    "shapes": [
+        {
+            "label": "polygon",
+            "points": coord_list,
+            "group_id": None,
+            "shape_type": "polygon",
+            "flags": {}
+        } for coord_list in coords
+    ],
+    "imagePath": "path/to/image/file.jpg",
+    "imageData": None,
+    "imageHeight": 256,
+    "imageWidth": 256
+}
+
+# save the labelme-compatible dictionary to a file
+with open("labelme_data2.json", "w") as f:
+    json.dump(labelme_dict, f)
+'''
+
+import json
+import numpy as np
+
+# define custom JSON encoder
+class NumpyEncoder(json.JSONEncoder):
+    """ Special json encoder for numpy types """
+    def default(self, obj):
+        if isinstance(obj, (np.int_, np.intc, np.intp, np.int8,
+                             np.int16, np.int32, np.int64, np.uint8,
+                             np.uint16,np.uint32, np.uint64)):
+            return int(obj)
+        elif isinstance(obj, (np.float_, np.float16, np.float32,
+                               np.float64)):
+            return float(obj)
+        elif isinstance(obj,(np.ndarray,)): #### This is the fix
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
+
+# create PolygonsOnImage object with multiple Polygon objects
+polygons = [Polygon([(0,0), (100,0), (100,100), (0,100)]), Polygon([(50,50), (75,50), (75,75), (50,75)])]
+polys = PolygonsOnImage(polygons, shape=(256, 256, 3))
+
+# convert the list of polygons to a list of lists of coordinates
+coords = [[[(x, y) for (x, y) in polygon.exterior]] for polygon in polygons]
+
+# create the labelme-compatible dictionary
+labelme_dict = {
+    "version": "4.5.7",
+    "flags": {},
+    "shapes": [
+        {
+            "label": "polygon",
+            "points": coord_list,
+            "group_id": None,
+            "shape_type": "polygon",
+            "flags": {}
+        } for coord_list in coords
+    ],
+    "imagePath": "path/to/image/file.jpg",
+    "imageData": None,
+    "imageHeight": 256,
+    "imageWidth": 256
+}
+
+# save the labelme-compatible dictionary to a file
+with open("labelme_data.json", "w") as f:
+    json.dump(labelme_dict, f, cls=NumpyEncoder)
---
+++ poly_aug.py
@@ -0,0 +1,138 @@
+import cv2
+import numpy as np
+import imgaug.augmenters as iaa
+import labelme
+import json
+from labelme import utils
+import os
+import numpy as np
+
+
+import json
+from imgaug.augmentables.polys import Polygon, PolygonsOnImage
+
+from imgaug import augmenters as iaa
+
+
+
+import json
+
+import numpy as np
+
+import cv2
+
+import imgaug as ia
+from imgaug.augmentables.polys import Polygon, PolygonsOnImage
+from imgaug2labelme import *
+
+def make_polys(json_file):
+    with open(json_file, "r") as js:
+        json_data = json.load(js)
+
+    polys = []
+
+    for shape in json_data['shapes']:
+        # This assert might be overkill but better safe that sorry ...
+        assert shape['shape_type'] == "polygon"
+        polys.append(Polygon(shape['points'], label=shape['label']))
+
+    img_shape = (json_data['imageHeight'], json_data['imageWidth'], 3)
+    polys_oi = PolygonsOnImage(polys, shape=img_shape)
+    return(polys_oi)
+
+
+quokka_img = cv2.imread("/home/moka/Akash/Seg-1001-1700_03042023/1003.jpg")
+
+polys_oi = make_polys("/home/moka/Akash/Seg-1001-1700_03042023/1003.json")
+
+# print(polys_oi)
+# # This is just to plot it ...
+# overlay_quokka = polys_oi.draw_on_image(quokka_img)
+
+# cv2.imwrite("overlaid_quokka.png", overlay_quokka)
+
+# for i, p in enumerate(polys_oi):
+#     overlay_quokka = p.draw_on_image(quokka_img, color=(0, 0, 255))
+#     cv2.imwrite(f"over_p{i}_quokka.png", overlay_quokka)
+
+
+def POI2labelmejson(img_name,poly, json_name) -> None:
+    # convert the PolygonsOnImage object to list of coordinates
+    polygons = poly.polygons
+    coords = [[(x, y) for (x, y) in polygon.exterior] for polygon in polygons]
+    labels = [polygon.label for polygon in polygons]
+    print("labels are>>>>>>>>>>", labels)
+    #poly.to_xy_array()
+
+    # create the labelme-compatible dictionary
+    labelme_dict = {
+        "version": "3.16.4", #4.5.7
+        "flags": {},
+        "shapes": [
+            {
+                "label": label_name,#"polygon",
+                "line_color": None,
+                "fill_color": None,
+                "points": coord_list,
+                "group_id": None,
+                "shape_type": "polygon",
+                "flags": {}
+            } for label_name, coord_list in zip(labels,coords)
+        ],
+        "lineColor": [0,225,0,128],
+        "fillColor": [225,0,0,128],
+        "imagePath": f"{img_name}",
+        "imageData": None,
+        "imageHeight": 2048,
+        "imageWidth": 2048
+    }
+
+    # save the labelme-compatible dictionary to a file
+    with open(f"{json_name}.json", "w") as f:
+        json.dump(labelme_dict, f, cls = NumpyEncoder)
+    return None
+
+
+my_augmenter = iaa.Sequential([
+    iaa.GaussianBlur((0.1, 5)),
+    iaa.Fliplr(0.5),
+    iaa.Flipud(0.5),
+    iaa.Rotate((-45,45))])
+
+img_path = "/home/moka/Akash/Seg-1001-1700_03042023/1003.jpg"
+json_path = "/home/moka/Akash/Seg-1001-1700_03042023/1003.json"
+
+def augmentation(img_path, json_path, my_augmenter,num_aug, aug_img_path):
+    quokka_img = cv2.imread(img_path)
+
+    polys_oi = make_polys(json_path)
+
+    # If you pass the arguments, it returns 2 elements
+    # - The augmented Image
+    # - The augmented polygons 
+    augmented = my_augmenter(image = quokka_img, polygons = polys_oi)
+    [type(x) for x in augmented]
+    # [<class 'numpy.ndarray'>, <class 'imgaug.augmentables.polys.PolygonsOnImage'>]
+
+    # So you can make a bunch of augmented image/polygon pairs
+    augmented_list = [my_augmenter(image = quokka_img, polygons = polys_oi) for _ in range(num_aug)]
+
+    # Now we just make the overlay for viz purposes
+    overlaid_images = [img for img, _ in augmented_list]
+
+    cv2.imwrite("overlaid_image.png", cv2.vconcat(overlaid_images))
+    i =0
+    for img, poly in augmented_list:
+        print("<<<<<<<<<<<<",aug_img_path+f"{img_path.split('/')[-1].split('.')[0]}_aug_{i}.png")
+        cv2.imwrite(aug_img_path+f"{img_path.split('/')[-1].split('.')[0]}_aug_{i}.png", img)
+
+        print(">>>>>>", poly.polygons)
+        print("\n\n\n")
+        POI2labelmejson(img_name=aug_img_path+f"{img_path.split('/')[-1].split('.')[0]}_aug_{i}.png",
+                        poly=poly, json_name=aug_img_path+ f"{img_path.split('/')[-1].split('.')[0]}_aug_{i}" )
+        i+=1
+
+
+
+if __name__ == "__main__":
+    augmentation(img_path, json_path, my_augmenter, 2, "./")
